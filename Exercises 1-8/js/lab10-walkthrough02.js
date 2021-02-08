abdd8b2178a8d665fe6c9ede3c5439bbe3c6b8ed
Rohan0310 <rpalkar@confederationcollege.ca>
2021-02-08 10:45:04 -0500

    Step 2 lab 4 exercise 2 final completion done

--- Exercises 1-8/js/lab10-walkthrough02.js
+++ Exercises 1-8/js/lab10-walkthrough02.js
@@ -4,3 +4,13 @@ msg.val("here is some new text");
 
 $('#first').html("new Title");
 
+$('#msgArea').val("My	class	is	"	+	$('#msgArea').attr("class") );
+
+var	buttons	=	$('button');
+buttons.css('background-color',	'red');
+
+var	temp	=	$('body');
+temp.css("background-color","ivory");
+
+$('.center-icons').addClass('selected');
+
