bbf4c36615bea83f156815cfbd71f2b7fe59d709
Neel Patel <npatel16@confederationcollege.ca>
2021-02-26 17:53:40 -0500

    step 3 project 1 completed

---
+++ project1/chapter10-project01.js
@@ -0,0 +1,12 @@
+$(document).ready(function()
+{
+  $("#thumbBox img").on('click',function()
+  {
+      
+      var alt = $(this).attr("alt");
+      var title = $(this).attr("title");
+      var src = $(this).attr("src"); 
+      var random = src.replace("small","medium");
+      var newsrc = $("#imgManipulated img").attr("src",random);
+  });
+}); 
