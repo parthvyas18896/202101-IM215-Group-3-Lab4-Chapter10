3332a222bc4d7e5222644d4ee85077385c551db5
parthvyas18896 <pvyas1@confederationcollege.ca>
2021-02-26 18:00:56 -0500

    project 1 step 5 done

--- project1/chapter10-project01.js
+++ project1/chapter10-project01.js
@@ -10,4 +10,61 @@ $(document).ready(function()
       var newsrc = $("#imgManipulated img").attr("src",random);
       $('figcaption').replaceWith($('<figcaption><em>'+ alt + '</em><br>' + title + '</figcaption>'))
   });
+
+var setFilterandwebkit = function () {
+
+    var filter = 'opacity('   + $('#sliderOpacity').attr('value')    + '%) ' +
+    'saturate('   + $('#sliderSaturation').attr('value') + ') ' +
+    'brightness(' + $('#sliderBrightness').attr('value') + '%) ' +
+    'hue-rotate('  + $('#sliderHue').attr('value')        + 'deg) ' +
+    'grayscale('  + $('#sliderGray').attr('value')       + '%) '  +
+    'blur('       + $('#sliderBlur').attr('value')       + 'px)';
+
+    $('#imgManipulated>img').css('filter',filter);
+    $('#imgManipulated>img').css('-webkit-ffilter',filter);
+        
+
+}
+$('#sliderOpacity').on('input', function (a) {
+  $('#sliderOpacity').attr('value', a.target.value);
+
+  setFilterandwebkit();
+
+});
+$('#sliderSaturation').on('input', function (a) {
+  $('#sliderSaturation').attr('value', a.target.value);
+
+  setFilterandwebkit();
+
+});
+$('#sliderBrightness').on('input', function (a) {
+  $('#sliderBrightness').attr('value', a.target.value);
+
+  setFilterandwebkit();
+
+});
+$('#sliderHue').on('input', function (a) {
+  $('#sliderHue').attr('value', a.target.value);
+
+  setFilterandwebkit();
+
+});
+$('#sliderGray').on('input', function (a) {
+  $('#sliderGray').attr('value', a.target.value);
+
+  setFilterandwebkit();
+
+});
+
+$('#sliderBlur').on('input', function (a) {
+  $('#sliderBlur').attr('value', a.target.value);
+
+  setFilterandwebkit();
+
+});
+
+
+
+
+
 }); 
